fe98b3b9c33351be9c9e38b12c31fbbf420fa551
DSV Siddharth <dsvsiddu2004@gmail.com>
2026-08-25 23:23:36 -0700

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://expenseflow-backend-n9cs.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
